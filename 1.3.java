be1d273233f4c4bd19406ad5675b1f9423542b15
Yadong Wen <ralph.wen@gmail.com>
2014-04-18 11:27:56 -0400

    refactor

--- 1.3.java
+++ 1.3.java
@@ -5,24 +5,26 @@ import java.util.HashMap;
 public class StringUtil {
     public static boolean arePermutations(String s1, String s2) {
         if(s1 == null || s2 == null) return false;
-        HashMap<Character, Integer> map1 = new HashMap<Character, Integer>();
-        for(int i = 0; i < s1.length(); i++) {
-            char c = s1.charAt(i);
-            if(!map1.containsKey(c)) {
-                map1.put(c, 1);
+        HashMap<Character, Integer> map1 = count(s1);
+        HashMap<Character, Integer> map2 = count(s2);
+        return checkEqual(map1, map2);
+    }
+    private static HashMap<Character, Integer> count(String s) {
+        if(s == null) return null;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!map.containsKey(c)) {
+                map.put(c, 1);
             } else {
-                map1.put(c, map1.get(c) + 1);
+                map.put(c, map.get(c) + 1);
             }
         }
-        HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
-        for(int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-            if(!map2.containsKey(c)) {
-                map2.put(c, 1);
-            } else {
-                map2.put(c, map2.get(c) + 1);
-            }
-        }
+        return map;
+    }
+    private static boolean checkEqual(HashMap<Character, Integer> map1, 
+        HashMap<Character, Integer> map2) {
+        if(map1 == null || map2 == null) return false;
         if(map1.size() != map2.size()) {
             return false;
         }
